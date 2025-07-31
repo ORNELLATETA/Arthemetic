235e89c03a46880aa0d7998378e9f30be5563bfe
Jean de Dieu Niyigena <niy011335@revenue.local>
2025-07-31 15:06:47 +0200

    addition

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+//addition
+        int number=1;
+        int numbers=2;
+        int sum= number+numbers;
+        System.out.println(sum);
+
+
+
+    }
+}
